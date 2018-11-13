25fce6db302e70069b3677cd66283d6670176f7a
Xavier <43478582+coder-log@users.noreply.github.com>
2018-11-13 08:06:23 +0100

    working on open/close menu js

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 
+let menuState = "closed";
+
 var openMenu = function(){
     document.getElementsByTagName("aside")[0].style.display = "inline";
-
+    
 }
+
+var closeMenu = function(){
+    document.getElementsByTagName("aside")[0].style.display = "none";
+}
+
 
 
 document.getElementById("burguer-menu").addEventListener("click", openMenu);
