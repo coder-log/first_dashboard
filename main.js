17fdbb6557c7b5c2fe01d6dfa6f7bbb36dc11d8d
Xavier <43478582+coder-log@users.noreply.github.com>
2018-11-13 06:38:04 +0100

    Added responsive menu, javascript not working yet

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var openMenu = function(){
+    document.getElementsByTagName('aside').style.display = "flex";
+
+}
+
+
+document.getElementById("burguer-menu").addEventListener("click", function(){
+    
+    console.log("This is working");
+});
