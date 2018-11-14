0b9d1b83fc13fee2ac1dd0ad358dcb19a85f779c
Xavier <43478582+coder-log@users.noreply.github.com>
2018-11-14 06:01:48 +0100

    improvements on responsive side menu some bugs on resize yet

--- main.js
+++ main.js
@@ -2,14 +2,27 @@
 let menuState = "closed";
 
 var openMenu = function(){
-    document.getElementsByTagName("aside")[0].style.display = "inline";
-    
+    if (menuState="closed") {
+        document.getElementsByTagName("aside")[0].style.display = "inline";
+        menuState = "open";
+    }
 }
 
 var closeMenu = function(){
-    document.getElementsByTagName("aside")[0].style.display = "none";
+    if (menuState ="open" && window.innerWidht < 660){
+        document.getElementsByTagName("aside")[0].style.display = "none";
+        menuState = "closed";
+    }
+}
+
+var resizeWindow = function(){
+    if (window.innerWidth > 660) {
+        openMenu;
+    }
+    return console.log(window.innerWidth);
 }
 
 
-
 document.getElementById("burguer-menu").addEventListener("click", openMenu);
+document.getElementsByTagName("aside")[0].addEventListener("click",closeMenu);
+window.addEventListener("resize", resizeWindow);
