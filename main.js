208df9ba5a3bd8118285be53af0eaa6add81a167
Xavier <43478582+coder-log@users.noreply.github.com>
2018-11-15 16:16:38 +0100

    Fixed bug on side menu

--- main.js
+++ main.js
@@ -1,28 +1,67 @@
 
-let menuState = "closed";
+//  var openMenu = function(){
+//     el = document.getElementsByTagName("aside")[0];
+//     el.style.display = "block";
+//     document.getElementById("burguer-menu").style.display = "none";
+// }
+
+// var closeMenu = function(){
+//     el = document.getElementsByTagName("aside")[0];
+//     el.style.display = "none";
+//     document.getElementById("burguer-menu").style.display = "inline";
+// }
+
+// document.getElementById("burguer-menu").addEventListener("click", openMenu);
+// document.getElementById("aside-logo-mb").addEventListener("click", closeMenu);
 
 var openMenu = function(){
-    if (menuState="closed") {
-        document.getElementsByTagName("aside")[0].style.display = "inline";
-        menuState = "open";
-    }
+    el = document.getElementsByTagName("aside")[0];
+    el.style.display = "block";
+    document.getElementById("burguer-menu").style.display = "none";
 }
 
 var closeMenu = function(){
-    if (menuState ="open" && window.innerWidht < 660){
-        document.getElementsByTagName("aside")[0].style.display = "none";
-        menuState = "closed";
-    }
+    el = document.getElementsByTagName("aside")[0];
+    el.style.display = "none";
+    document.getElementById("burguer-menu").style.display = "inline";
 }
 
-var resizeWindow = function(){
-    if (window.innerWidth > 660) {
+var openMenuOnResize = function(){
+    const mq = window.matchMedia("(min-width: 660px)");
+    if (mq.matches) {
         openMenu;
     }
-    return console.log(window.innerWidth);
 }
 
-
 document.getElementById("burguer-menu").addEventListener("click", openMenu);
-document.getElementsByTagName("aside")[0].addEventListener("click",closeMenu);
-window.addEventListener("resize", resizeWindow);
+document.getElementById("aside-logo-mb").addEventListener("click", closeMenu);
+window.addEventListener('resize', openMenuOnResize);
+
+/*Resize Window */
+ if (matchMedia) {
+    const mq = window.matchMedia("(min-width: 660px)");
+    mq.addListener(WidthChange);
+    WidthChange(mq);
+    }
+    
+ //    media query change
+     function WidthChange(mq) {
+    if (mq.matches) { openMenu;
+     // window width is at least 660px
+	 document.getElementById('sidemenu').style.display='block'
+	 
+	 
+    } else {
+     // window width is less than 660px
+	 document.getElementById('sidemenu').style.display='none';
+	 var btn=document.getElementById('burguer-menu');
+	 if(isHidden(btn)){
+		 btn.style.display='inline';
+		 }
+     }
+    
+    }
+function isHidden(el) {
+    var style = window.getComputedStyle(el);
+    return (style.display === 'none')
+}
